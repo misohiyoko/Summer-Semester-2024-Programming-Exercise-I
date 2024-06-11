e0887926e3754367be3ce7ed25a7a51c4c0e29d8
misohiyoko <misohiyoko@gmail.com>
2024-06-11 22:47:08 +0900

    up

--- src/hoge/he.c
+++ src/hoge/he.c
@@ -1,11 +1,24 @@
 //
 // Created by genta on 2024/06/08.
 //
-#include <stdio.h>
-#define MAXDATASIZE 100000
-int main(int argc, char *argv[]){
 
+#include <stdio.h>
+#define N 10
+void f(int x[], int n){
+    if(n==10){
+        return;
+    }else{
+        printf("%d",x[n]);
+
+        return f(x,n+1);
+    }
+}
+int main(){
+    int array[10];
+    int i;
+    for (i=0;i<10;i++){
+        array[i]=i;
+    }
+    f(array,0);
     return 0;
 }
-
-
